50956c96eb22fc0db5f16af0e824cf20a2ae4749
lchyun23 <lchyun23@gmail.com>
2024-09-24 01:07:07 +0900

    feat: 메서드 정의 및 호출

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+// 메서드는 클래스의 행동을 정의한다. 
+
+public class Calculator {
+    // 덧셈 메서드
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // 뺄셈 메서드
+    int subtract(int a, int b) {
+        return a - b;
+    }
+}
--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        // 생성자를 사용하여 객체 초기화
-        Person person1 = new Person("John", 30);
-        
-        // 메서드 호출
-        person1.sayHello();
+       // Calculator 객체 생성
+       Calculator calculator = new Calculator();
+
+       // 메서드 호출
+      int sum = calculator.add(5, 3);
+      int difference = calculator.subtract(5, 3);
+
+       // 결과 출력
+       System.out.println("Sum: " + sum);
+       System.out.println("Difference " + difference);
     }
 }
+// 메서드는 매개변수를 받아 결과를 반환하는 방식으로 동작한다.
