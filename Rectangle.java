70f1092bb3c074873037d805dae7c3f0ede66bfa
lchyun23 <lchyun23@gmail.com>
2024-09-24 01:15:37 +0900

    feat: 변환값이 있는 메서드

---
+++ Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle {
+    int length;
+    int width;
+
+    // 면적을 계산하는 메서드 (반환값 있음)
+    int calculateArea() {
+        return length * width;
+    }
+}
--- main.java
+++ main.java
@@ -1,10 +1,14 @@
 public class main {
     public static void main(String[] args) {
-       // Person 객체 생성
-       Person person1 = new Person();
-       person1.name = "Alice";
+        // Rectangle 객체 생성 및 초기화
+        Rectangle rectangle = new Rectangle();
+        rectangle.length = 5;
+        rectangle.width = 3;
 
-       // 지역 변수로 메서드 실행
-       person1.introduce("greeting");
+        // 메서드 호출 및 반환값 저장
+        int area = rectangle.calculateArea();
+
+        // 반환값 면적 출력
+        System.out.println("Area of rectangle: " + area);
     }
 }
