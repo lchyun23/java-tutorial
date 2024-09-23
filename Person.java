9ce2d9000583e1dba4dff1334c1037a9b9ff31bc
lchyun23 <lchyun23@gmail.com>
2024-09-24 00:55:22 +0900

    feat: 클래스 정의, 객체 생성, 메서드 호출

--- Person.java
+++ Person.java
@@ -1,10 +1,12 @@
+// 클래스는 객체를 생성하기 위한 템플릿이다.
 class Person {
     // 클래스 필드(변수)
     String name;
     int age;
 
-    // 클래스 메서드
+    // 클래스 메서드 : 사람의 이름을 출력하는 간단한 메서드
     void sayHello() {
         System.out.println("Hello, my name is" + name);
     }
 }
+
---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        // 객체 생성
+        Person person1 = new Person();
+        person1.name = "John";
+        person1.age = 30;
+        
+        // 메서드 호출
+        person1.sayHello();
+    }
+}
