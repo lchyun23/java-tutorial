2c4c6a8885b3fec69e04c53f5ba0956f59a12106
lchyun23 <lchyun23@gmail.com>
2024-09-24 23:49:03 +0900

    feat: 생활코딩 instance

--- ClassApp.java
+++ ClassApp.java
@@ -1,9 +1,12 @@
 // 클래스 사용법
-
+// 일회용
 public class ClassApp {
     public static void main(String[] args) {
         System.out.println(Math.PI);
         System.out.println(Math.floor(1.6));
         System.out.println(Math.ceil(1.6));
+
+        
+
     }
 }
---
+++ InstanceApp.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// class를 복제한 인스턴스를 만드는 경우: 일회용이 아니라 계속해서 사용해야 하는 경우이다.
+public class InstanceApp {
+    public static void main(String[] args) throws  IOException{
+        
+        PrintWriter p1 = new PrintWriter("result1.txt");
+        p1.write("Hello 1");
+        p1.close();
+
+        PrintWriter p2 = new PrintWriter("result2.txt");
+        p2.write("Hello 2");
+        p2.close();
+
+    }
+}
