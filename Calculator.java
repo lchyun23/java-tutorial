3ad4b7a92f409e1b4bc18ad133c0d51a5a48ac7d
lchyun23 <lchyun23@gmail.com>
2024-09-24 11:58:30 +0900

    feat: javac 연습

--- Calculator.java
+++ Calculator.java
@@ -8,9 +8,10 @@ public class Calculator {
     public int add(int a, int b, int c) {
         return a + b + c;
     }
-
-    // 세 번째 메서드: 두 개의 실수(double)을 더하는 메서드 작성
+        // 세 번째 메서드: 세 개의 실수(double)을 더하는 메서드 작성
     public double add(double a, double b, double c) {
         return a + b + c;
     }
 }
+
+
