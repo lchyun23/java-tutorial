04ed8b96b2c9c51ff947c27867349b98ebf65a38
lchyun23 <lchyun23@gmail.com>
2024-09-24 23:29:35 +0900

    feat: java 연습

---
+++ Sample1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Sample1 {
+    
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+        System.out.println("₩100 을 받았다");
+        System.out.println("내일 또 만나요.");
+        System.out.println("1 \t 2 \t 3");
+        
+    }
+}
